9396b476be812229c9b6ab257bd680ad9e367935
smg0725 <smg0725@naver.com>
2024-02-07 02:17:09 +0900

    login

--- pages/SignUp/index.tsx
+++ pages/SignUp/index.tsx
@@ -1,16 +1,53 @@
 import React, { useCallback, useState } from 'react';
-import {Form, Label, Input, LinkContainer, Button, Header} from './styles';
+import {Form, Error, Label, Input, LinkContainer, Button, Header, Success} from './styles';
+import axios from 'axios';
+import useInput from "@hooks/useInput";
+import {Link} from "react-router-dom";
 
 const SignUp = () => {
-    const[email] = useState('');
-    const[nickname] = useState('');
-    const[password] = useState('');
-    const[passwordCheck] = useState('');
-    const onChangeEmail = useCallback(()=>{},[]);
-    const onChangeNickname = useCallback(()=>{},[]);
-    const onChangePassword = useCallback(()=>{},[]);
-    const onChangePasswordCheck = useCallback(()=>{},[]);
-    const onSubmit = useCallback(()=>{},[]);
+    const[email,onChangeEmail] = useInput('');
+    const[nickname,onChangeNickname] = useInput('');
+    const[password,setPassword] = useState('');
+    const[passwordCheck,setPasswordCheck] = useState('');
+    const [mismatchError, setMismatchError] = useState(false);
+    const [signUpError, setSignUpError] = useState("");
+    const [signUpSuccess, setSignUpSuccess] = useState(false);
+
+
+    const onChangePassword = useCallback((event:any)=>{
+        setPassword(event.target.value);
+        setMismatchError(event.target.value !== password);
+    },[passwordCheck]);//패스워드 쳌이 바뀔때 같은지 다른지 확인할거니까?
+
+    const onChangePasswordCheck = useCallback((event:any)=>{
+        setPasswordCheck(event.target.value);
+        setMismatchError(event.target.value !== password);
+    },[password]);//패스워드가 바뀔때 확인하니까?
+
+    const onSubmit = useCallback((event:any)=>{
+        event.preventDefault();
+        setSignUpError("");//비동기에서 state를 설정할때 이렇게 한번씩 초기화 해주고 사용해야
+        setSignUpSuccess(false);//  전에 사용하던 스테이트를 그대로 사용하는것을 방지할 수 있다
+
+        if(!mismatchError && nickname){
+            console.log("서버로 회원가입!")
+            axios.post("http://localhost:3095/api/users",{
+                email,
+                nickname,
+                password,
+            })
+                .then((response:any)=>{
+                    console.log(response);
+                    setSignUpSuccess(true);
+                })
+                .catch((error:any)=>{
+                    console.log(error.response);
+                    setSignUpError(error.response.data);
+                })
+                .finally(()=>{})
+
+        }
+    },[email,nickname,password,password,mismatchError]);
 
     // const { data: userData } = useSWR('/api/users', fetcher);
     // const [signUpError, setSignUpError] = useState(false);
@@ -96,16 +133,16 @@ const SignUp = () => {
                 onChange={onChangePasswordCheck}
               />
             </div>
-            {/* {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+             {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
             {!nickname && <Error>닉네임을 입력해주세요.</Error>}
-            {signUpError && <Error>이미 가입된 이메일입니다.</Error>}
-            {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>} */}
+            {signUpError && <Error>{signUpError}</Error>}
+            {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
           </Label>
           <Button type="submit">회원가입</Button>
         </Form>
         <LinkContainer>
           이미 회원이신가요?&nbsp;
-          <a href="/login">로그인 하러가기</a>
+          <Link to="/login">로그인 하러가기</Link>
         </LinkContainer>
       </div>
     );
