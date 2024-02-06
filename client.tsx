f8f9009dd12a948ae9fb45769926c01d87ddf65a
smg0725 <smg0725@naver.com>
2024-02-06 14:38:50 +0900

    realNew

---
+++ client.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import {render} from 'react-dom';
+import App from '@layouts/App';
+import { BrowserRouter } from 'react-router-dom';
+render(<BrowserRouter><App /></BrowserRouter>, document.querySelector('#app'));
